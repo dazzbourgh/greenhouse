51b6fa8b3721a606149210a7ecca514eece4c685
Leonid Borisevich <leonidborisevich00@gmail.com>
2020-05-24 12:16:33 +0300

    used constants instead of hard coding

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from src.actions.action_types import SET_TEMPERATURE
 from src.middleware.middleware import logging_middleware
 from src.reducers.reducers import temperature
 from src.store.action import Action
@@ -10,4 +11,4 @@ if __name__ == '__main__':
         },
         [temperature],
         apply_middleware([logging_middleware()]))
-    store.dispatch(Action('TEMP', {'temperature': 80}))
+    store.dispatch(Action(SET_TEMPERATURE, {'temperature': 80}))
--- src/reducers/reducers.py
+++ src/reducers/reducers.py
@@ -1,8 +1,9 @@
+from src.actions.action_types import SET_TEMPERATURE
 from src.store.action import Action
 
 
 def temperature(action: Action, state=70) -> int:
-    if action.type == 'TEMP':
+    if action.type == SET_TEMPERATURE:
         return action.payload['temperature']
     else:
         return state
